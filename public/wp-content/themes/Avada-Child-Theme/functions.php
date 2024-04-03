cd63c99af7c8d410bd7bad456630f22d5cf6fff7
Weston Wei <gxbsst@gmail.com>
2024-04-03 23:59:05 +0800

    update local

--- public/wp-content/themes/Avada-Child-Theme/functions.php
+++ public/wp-content/themes/Avada-Child-Theme/functions.php
@@ -10,3 +10,48 @@ function avada_lang_setup() {
 	load_child_theme_textdomain( 'Avada', $lang );
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+
+function custom_error_logging($message, $log_type = 'error') {
+    switch ($log_type) {
+        case 'error':
+            error_log('[ERROR] ' . $message);
+            break;
+        case 'warning':
+            error_log('[WARNING] ' . $message);
+            break;
+        case 'info':
+            error_log('[INFO] ' . $message);
+            break;
+        default:
+            error_log('[LOG] ' . $message);
+            break;
+    }
+}
+
+add_action('wp_footer', function() {
+    global $wpdb;
+    if (defined('WP_DEBUG_LOG') && WP_DEBUG_LOG && defined('WP_DEBUG') && WP_DEBUG) {
+        foreach ($wpdb->queries as $query) {
+            custom_error_logging('Query: ' . $query[0] . ' - Time: ' . $query[1]);
+        }
+    }
+});
+
+
+add_action('shutdown', function() {
+    global $wpdb;
+    $log_file = '/opt/homebrew/var/log/wordpress/db.log';
+    $log_data = '';
+
+    if (defined('SAVEQUERIES') && SAVEQUERIES && !empty($wpdb->queries)) {
+        foreach ($wpdb->queries as $query) {
+            $query_time = $query[1];
+            $query_callstack = $query[2];
+            $query_sql = $query[0];
+            $log_data .= sprintf("[%s] %s - %s\n", date('Y-m-d H:i:s'), $query_sql, $query_time);
+        }
+        file_put_contents($log_file, $log_data, FILE_APPEND);
+    }
+});
+
